afa9bf09c042733aa33435dca7caf0aa3e1befc8
SEUNG BEEN LEE <seungbeenlee@SEUNGs-MacBook-Pro-2.local>
2020-06-20 17:07:45 +0900

    Add files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="pypgx",
+    version="0.0.1",
+    author='Seung-been "Steven" Lee',
+    author_email="sbstevenlee@gmail.com",
+    description="A Python package for PGx research",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypgx/pypgx/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
