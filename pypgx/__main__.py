8b91a680d0fce88ce09701365440958a50958ceb
SEUNG BEEN LEE <seungbeenlee@SEUNGs-MacBook-Pro-2.local>
2020-06-21 20:36:46 +0900

    Add files

---
+++ pypgx/__main__.py
@@ -0,0 +1,5 @@
+def main():
+    print("Who called me?")
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -20,4 +20,5 @@ setup(
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
+    entry_points={"console_scripts": ["pypgx=pypgx.__main__:main"]}
 )
