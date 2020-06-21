cb41f11a7a64f95077b9adddcad1b482055e8a79
SEUNG BEEN LEE <seungbeenlee@SEUNGs-MacBook-Pro-2.local>
2020-06-21 20:49:31 +0900

    Add files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+requirements = ["requests>=2"]
 
 setup(
     name="pypgx",
