a1ec0d9a7ea40b7459dafd46592b0cd83337e037
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2020-06-26 14:35:06 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,12 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = ".rst"
+
 # The master toctree document.
 master_doc = "index"
 
