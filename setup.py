16fbea98346d344d52588f5f50db632659e07806
SEUNG BEEN LEE <seungbeenlee@SEUNGs-MacBook-Pro-2.local>
2020-06-29 12:14:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("README.rst", "r") as readme_file:
-    readme = readme_file.read()
-
 exec(open("pypgx/version.py").read())
 
 requirements = ["requests>=2", "pandas>=1.0.0", "bs4>=0.0.1",
@@ -14,7 +11,7 @@ setup(
     author='Seung-been "Steven" Lee',
     author_email="sbstevenlee@gmail.com",
     description="PyPGx is a Python package for pharmacogenomics (PGx) research",
-    long_description=readme,
+    long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
     url="https://github.com/pypgx/pypgx/",
     packages=find_packages(),
