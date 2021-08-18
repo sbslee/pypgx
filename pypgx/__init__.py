f433979f8fb1b111a7db0f78120dbcc771ec2365
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2021-08-18 16:02:25 +0900

    Update API

--- pypgx/__init__.py
+++ pypgx/__init__.py
@@ -1,8 +1,9 @@
 from .api.utils import (
     get_activity_score,
     get_phenotype,
-    load_gene_table,
     load_activity_table,
+    load_equation_table,
+    load_gene_table,
     load_phenotype_table,
 )
 
--- pypgx/api/utils.py
+++ pypgx/api/utils.py
@@ -107,28 +107,25 @@ def get_phenotype(gene, a, b):
     'Intermediate Metabolizer'
     """
     _check_gene(gene)
+
     df = load_phenotype_table()
-    df = df[df.Gene == gene]
-    l = [f'{a}/{b}', f'{b}/{a}']
-    return df[df.Diplotype.isin(l)].Phenotype.values[0]
 
-def load_gene_table():
-    """
-    Return the gene table.
+    if gene in df.Gene.unique():
+        df = df[df.Gene == gene]
+        l = [f'{a}/{b}', f'{b}/{a}']
+        i = df.Diplotype.isin(l)
+        return df[i].Phenotype.values[0]
 
-    Returns
-    -------
-    pandas.DataFrame
-        Activity table.
+    df = load_equation_table()
 
-    Examples
-    --------
+    if gene in df.Gene.unique():
+        def one_row(r, score):
+            return eval(r.Equation)
+        score = get_activity_score(gene, a) + get_activity_score(gene, b)
+        i = df.apply(one_row, args=(score,), axis=1)
+        return df[i].Phenotype.values[0]
 
-    >>> import pypgx
-    >>> df = pypgx.load_activity_table()
-    """
-    b = BytesIO(pkgutil.get_data(__name__, 'data/gene-table.csv'))
-    return pd.read_csv(b)
+    raise ValueError('Something went wrong')
 
 def load_activity_table():
     """
@@ -146,6 +143,42 @@ def load_activity_table():
     >>> df = pypgx.load_activity_table()
     """
     b = BytesIO(pkgutil.get_data(__name__, 'data/activity-table.csv'))
+    return pd.read_csv(b)
+
+def load_equation_table():
+    """
+    Return the phenotype equation table.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Phenotype equation table.
+
+    Examples
+    --------
+
+    >>> import pypgx
+    >>> df = pypgx.load_activity_table()
+    """
+    b = BytesIO(pkgutil.get_data(__name__, 'data/equation-table.csv'))
+    return pd.read_csv(b)
+
+def load_gene_table():
+    """
+    Return the gene table.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Activity table.
+
+    Examples
+    --------
+
+    >>> import pypgx
+    >>> df = pypgx.load_activity_table()
+    """
+    b = BytesIO(pkgutil.get_data(__name__, 'data/gene-table.csv'))
     return pd.read_csv(b)
 
 def load_phenotype_table():
