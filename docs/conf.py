600060951446176389e8c531bbfa2fd6d2cfe920
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2021-08-20 13:50:35 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
@@ -37,8 +37,6 @@ extensions = [
 autodoc_default_options = {
     'autosummary': True,
 }
-
-napoleon_google_docstring = True
 
 napoleon_use_param = False
 
