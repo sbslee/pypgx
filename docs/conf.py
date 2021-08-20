9094306357067d1c5919607dd650fa44d6497cf7
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2021-08-20 14:04:56 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,15 +43,6 @@ napoleon_use_param = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
-
-# The master toctree document.
-master_doc = 'index'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
